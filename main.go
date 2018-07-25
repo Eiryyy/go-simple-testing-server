e6c772c01a39057e62b43fba6acdd7556521511e
Eiryyy <xxx.eiryklav@gmail.com>
2018-07-25 17:53:12 +0900

    returns json

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
+	"net/url"
+	"strconv"
 )
 
+type s struct {
+	Counter int `json:"counter"`
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
+	u, _ := url.Parse(r.RequestURI)
+	counter, _ := strconv.Atoi(u.Query().Get("c"))
+	j := s{Counter: counter}
+	json, _ := json.Marshal(j)
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
+	w.Write(json)
 }
 
 func main() {
